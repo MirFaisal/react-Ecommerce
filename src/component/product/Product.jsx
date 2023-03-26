88a6a63a24e2267c3a2467fe70ad6dfdd910cb93
mir_faisal <mirfaisal08@gmail.comm>
2023-03-26 16:44:05 +0600

    added font awesome npm

--- src/component/product/Product.jsx
+++ src/component/product/Product.jsx
@@ -1,7 +1,14 @@
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Product.css";
-const Product = (props) => {
-  const { name, price, ratings, seller, img } = props.product;
+const Product = ({ handelAddToCart, product }) => {
+  /*
+  another method to apply props
+  const { handelAddToCart, product }=props;
+  */
+  const { name, price, ratings, seller, img } = product;
+
   return (
     <div className="card">
       <div className="product-img">
@@ -15,7 +22,10 @@ const Product = (props) => {
         <p className="manufacturer">Manufacturer : {seller}</p>
         <p className="rating">Rating : {ratings}</p>
       </div>
-      <button className="btn-primary">Add to Cart</button>
+      <button onClick={() => handelAddToCart(product)} className="btn-primary">
+        <p> Add to Cart</p>
+        <FontAwesomeIcon icon={faShoppingCart} />
+      </button>
     </div>
   );
 };
--- src/component/products/Products.jsx
+++ src/component/products/Products.jsx
@@ -3,21 +3,39 @@ import Product from "../product/Product";
 import "./Products.css";
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
+  const handelAddToCart = (product) => {
+    console.log(product);
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
   return (
     <div className="shop-container">
       <div className="products-wrapper">
         {products.map((product) => (
-          <Product key={product.id} product={product} />
+          <Product
+            key={product.id}
+            product={product}
+            handelAddToCart={handelAddToCart}
+          />
         ))}
       </div>
       <div className="order-summary">
         <h2>Order ummary</h2>
+        <div className="cart">
+          <p>Selected Items: {cart.length}</p>
+          <p>Total Price: {}</p>
+          <p>Total Shipping Charge: {}</p>
+          <p>Tax: {}</p>
+          <h3>Grand Total: {}</h3>
+        </div>
       </div>
     </div>
   );
